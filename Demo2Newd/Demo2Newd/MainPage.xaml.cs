f536380b01b12eb1968f8b2b17165e3f2eb89584
dianii <38058753+dianii@users.noreply.github.com>
2018-04-07 13:35:51 -0500

    Implementacion View

--- Demo2Newd/Demo2Newd/MainPage.xaml.cs
+++ Demo2Newd/Demo2Newd/MainPage.xaml.cs
@@ -13,5 +13,51 @@ namespace Demo2Newd
 		{
 			InitializeComponent();
 		}
-	}
+        private void picMusik_Changed(object sender, EventArgs e)
+        {
+            var elementoSeleccionado = picMusik.SelectedItem.ToString();
+            DisplayAlert("Musica", elementoSeleccionado, "Aceptar");
+        }
+        private void btnSimular_Clicked(object sender, EventArgs e)
+        {
+            var progreso = progProceso.Progress;
+            if (progreso == 1)
+            {
+                progProceso.ProgressTo(.1, 250, Easing.Linear);
+            }
+            else
+            {
+                progProceso.ProgressTo(.1, 250, Easing.Linear);
+            }
+        }
+        private void sbNombre_Pressed(object sender, EventArgs e)
+        {
+            DisplayAlert("Search Bar", "Estoy Buscando...", "Aceptar");
+        }
+        private void sbNombre_Changed(object sender, EventArgs e)
+        {
+            DisplayAlert("Search Bar", "Estoy Escribiendo...", "Aceptar");
+        }
+
+        private void sldEdad_Changed(object sender, EventArgs e)
+        {
+            lbEdad.Text = sldEdad.Value.ToString();
+        }
+        private void stpCantidad_Changed(object sender, EventArgs e)
+        {
+            lbCantidad.Text = stpCantidad.Value.ToString();
+        }
+
+        private void swDec_Toggled(object sender, EventArgs e)
+        {
+            if (swDec.IsToggled)
+            {
+                DisplayAlert("Mensaje", "Esta Activo", "Aceptar");
+            }
+            else
+            {
+                DisplayAlert("Mensaje", "No esta Activo", "Aceptar");
+            }
+        }
+    }
 }
